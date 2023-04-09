812a66f691152f1476fa9a2e6663e72986770b0a
Wakayama_Tatsuhiro <tatsuhiro.0323@gmail.com>
2023-04-09 22:50:30 +0900

    First Commit

---
+++ src/main/java/com/tuyano/springboot/HelloController.java
@@ -0,0 +1,25 @@
+package com.tuyano.springboot;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/")
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		return mav;
+	}
+	@RequestMapping("/other")
+	public String other() {
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/home")
+	public String home() {
+		return "forward:/";
+	}
+}
